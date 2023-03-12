f28d29bdae4af99d4081bbffe5d36a60594a43bd
cameronbeach12 <cameronbeach12@gmail.com>
2023-03-12 19:12:22 -0400

    Base queue system set up, started implementing class based animations

--- Player.gd
+++ Player.gd
@@ -25,14 +25,18 @@ enum CLASS {
 	PALADIN
 }
 
-var _class = CLASS.DEFAULT
+#QUEUE INTERRUPT PRIORITY - DASH, MOVEMENT, SKILL
+#DASHING CLEARS THE QUEUE
+
+var _class = CLASS.GUNSLINGER
 var _damage_type = DAMAGE_TYPE.DEFAULT
 var _state = STATE.IDLE
 const SPEED = 150.0
 var target = position
 var prev_velocity = Vector2()
 @onready var animator = $AnimatedSprite2D
-	
+var queue = []
+
 func animation():
 	if _state == STATE.IDLE:
 		animator.play("idle_down")
@@ -61,34 +65,90 @@ func CheckSpellAnim():
 	elif animator.animation == "skill_6_GS":
 		print(animator.animation)
 		_state = STATE.IDLE
+		
+	if !queue.is_empty() and _class == CLASS.GUNSLINGER:
+		if queue[0] != "movement":
+			if queue[0].left(5) == "skill":
+				_state = STATE.CASTING
+				animator.play(queue[0] + "_GS")
+				queue.clear()
 
 func _physics_process(delta):
 	if Input.is_mouse_button_pressed(MOUSE_BUTTON_RIGHT):
 		target = get_global_mouse_position()
+		if !queue.is_empty():
+			if queue[0] != "movement":
+				queue.clear()
+				queue.insert(0, "movement")
+		elif queue.is_empty() and _state == STATE.CASTING:
+			queue.append("movement")
 		
-	if Input.is_action_just_pressed("skill1"):
+	if Input.is_action_just_pressed("skill1") and _state != STATE.CASTING:
 		animator.play("skill_1_GS")
+		target = position
 		_state = STATE.CASTING
-	elif Input.is_action_just_pressed("skill2"):
+	elif Input.is_action_just_pressed("skill1") and _state == STATE.CASTING:
+		if queue.is_empty():
+			queue.append("skill_1")
+		else:
+			queue.insert(0, "skill_1")
+				
+	if Input.is_action_just_pressed("skill2") and _state != STATE.CASTING:
 		animator.play("skill_2_GS")
+		target = position
 		_state = STATE.CASTING
-	elif Input.is_action_just_pressed("skill3"):
+	elif Input.is_action_just_pressed("skill2") and _state == STATE.CASTING:
+		if queue.is_empty():
+			queue.append("skill_2")
+		else:
+			queue.insert(0, "skill_2")
+				
+	if Input.is_action_just_pressed("skill3") and _state != STATE.CASTING:
 		animator.play("skill_3_GS")
+		target = position
 		_state = STATE.CASTING
-	elif Input.is_action_just_pressed("skill4"):
+	elif Input.is_action_just_pressed("skill3") and _state == STATE.CASTING:
+		if queue.is_empty():
+			queue.append("skill_3")
+		else:
+			queue.insert(0, "skill_3")
+				
+	if Input.is_action_just_pressed("skill4") and _state != STATE.CASTING:
 		animator.play("skill_4_GS")
+		target = position
 		_state = STATE.CASTING
-	elif Input.is_action_just_pressed("skill5"):
+	elif Input.is_action_just_pressed("skill4") and _state == STATE.CASTING:
+		if queue.is_empty():
+			queue.append("skill_4")
+		else:
+			queue.insert(0, "skill_4")
+				
+	if Input.is_action_just_pressed("skill5") and _state != STATE.CASTING:
 		animator.play("skill_5_GS")
+		target = position
 		_state = STATE.CASTING
-	elif Input.is_action_just_pressed("skill6"):
+	elif Input.is_action_just_pressed("skill5") and _state == STATE.CASTING:
+		if queue.is_empty():
+			queue.append("skill_5")
+		else:
+			queue.insert(0, "skill_5")
+	if Input.is_action_just_pressed("skill6") and _state != STATE.CASTING:
 		animator.play("skill_6_GS")
+		target = position
 		_state = STATE.CASTING
+	elif Input.is_action_just_pressed("skill6") and _state == STATE.CASTING:
+		if queue.is_empty():
+			queue.append("skill_6")
+		else:
+			queue.insert(0, "skill_6")
+				
 	
 	velocity = position.direction_to(target) * SPEED
 	
 	if position.distance_to(target) > SPEED * delta:
-		move_and_slide()
+		if _state != STATE.CASTING:
+			queue.clear()
+			move_and_slide()
 	
 	if (abs(target.x - position.x) < (SPEED * delta) and abs(target.y - position.y) < (SPEED * delta)) and _state != STATE.CASTING:
 		_state = STATE.IDLE
@@ -97,7 +157,7 @@ func _physics_process(delta):
 			_state = STATE.WALKING
 	
 	animation()
-	print(_state)
+	print(queue)
 
 func _on_animated_sprite_2d_animation_looped():
 	CheckSpellAnim()
